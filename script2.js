2510494722e068a2ee52aec4053d044b80fd59d3
GuillaumeC0lin <90337894+GuillaumeC0lin@users.noreply.github.com>
2021-11-14 10:39:18 +0100

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,13 +1,55 @@
+var i = 0;
 function check()
 {
 	var a = document.getElementById('login').value;
 	var b = document.getElementById('pass').value;
 
-	if(a == "Admin_GSB" && b =="admin")
+	if(i<3)
 	{
-		document.getElementById("a1").className =("show");
-		document.getElementById("a2").className =("show");
-		document.getElementById("a3").className =("show");
-
+		if(a == "Admin_GSB" && b =="admin")
+		{
+			document.getElementById("a1").className =("show");
+			document.getElementById("a2").className =("show");
+			document.getElementById("a3").className =("show");
+		}
+		else
+		{
+			i=i+1;
+		}
 	}
+	else
+	{
+		alert("nombre d'essais depassé! veuillez recharger la page");
+	}
+}
+function exo_js_1()
+{
+	var i = 0;
+	var tab = new Array();
+	var j = prompt("donner la taille du tableau");
+	var res = "|";
+	while(i<j)
+	{
+		tab[i] = prompt("donnez une valeur");
+		i++;
+	}
+	for(var a=0;a<j;a++)
+	{
+		res = res +tab[a]+"|"
+		document.getElementById("out").innerHTML=(res);
+	}
+
+}
+function exo_js_2()
+{
+	var a = 0;
+	var i = 0;
+	var j = prompt("donner la valeur de fin");
+	while(i<=j)
+	{
+		a = a+i;
+		i++;
+	}
+		document.getElementById("out").innerHTML=(a);
+
 }
